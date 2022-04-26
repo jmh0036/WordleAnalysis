863313e0f279a1e94af0f53ccf3f2de702d72f8d
jmh0036 <jmh0036@tigermail.auburn.edu>
2022-04-26 14:14:10 -0400

    Commenting Code

--- WordleWords.py
+++ WordleWords.py
@@ -3,52 +3,56 @@ import matplotlib.pyplot as plt
 
 WordleLength = 5
 
+# Define Dictionaries of letters w.r.t. index in array (1 counting)
 LettersDict = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26}
-
 InvLettersDict = {v:k for k,v in LettersDict.items()}
 
+# Create a list of WordleLength letter words
 WordleWords = []
 for word in set(twl.iterator()):
     if len(word) == WordleLength:
         WordleWords.append(word)
-
 print('Five Letter Words:', len(WordleWords))
 
+# Input: This function takes on an integer between 0 and WordleLength-1 
+# Output: a list of length 26 that counts the number of times each letter appears in that position
 def LetterCountPosition(position):
     LocalLetterCount = [0] * 26
     for word in WordleWords:
         LocalLetterCount[ LettersDict[word[position]] - 1 ] += 1
     return LocalLetterCount
 
+# Count the number of times each letter appears in the dictionary
+# Note: If a letter appears multiple times within a word, it will be counted multiple times
 LetterCount = [0] * 26
 for word in WordleWords:
     for letter in word:
         LetterCount[LettersDict[letter]-1] += 1
 
-LettersInWordCount = [0] * 26
-for word in WordleWords:
-    for letter in set(word):
-        LettersInWordCount[LettersDict[letter]-1] += 1
-
 print('')
 
 print('Occurances of each letter Accounting for duplication within a word:   ', LetterCount)
 
+# Define the Graphic
 axletters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-plt.bar(axletters, LetterCount, color ='forestgreen',
-        width = 0.4)
-
+plt.bar(axletters, LetterCount, color ='forestgreen', width = 0.4)
 plt.xlabel("Letters")
 plt.ylabel("Appearances in Dictionary")
 plt.title("Letter Appearance Accounting for Duplication Within a Word")
 plt.savefig('LettersAccountingForMultiplicity')
 plt.close()
 
+# Count the number of times each letter appears in a word the dictionary
+# Note: If a letter appears multiple times within a word, it will only be counted once
+LettersInWordCount = [0] * 26
+for word in WordleWords:
+    for letter in set(word):
+        LettersInWordCount[LettersDict[letter]-1] += 1
+
 print('Occurances of each letter Not Accounting or duplication within a word:', LettersInWordCount)
 
-plt.bar(axletters, LettersInWordCount, color ='forestgreen',
-        width = 0.4)
-
+# Define Graphic
+plt.bar(axletters, LettersInWordCount, color ='forestgreen', width = 0.4)
 plt.xlabel("Letters")
 plt.ylabel("Words with that letter in Dictionary")
 plt.title("Letter Appearance Not Accounting for Duplication Within a Word")
@@ -57,6 +61,7 @@ plt.close()
 
 print('')
 
+# Determine the distribution of letters in the dictionary (counting multiples within a word)
 MaxLetterCount = max(LetterCount)
 print('The letter', InvLettersDict[LetterCount.index(MaxLetterCount)+1], 'achieves occurs', MaxLetterCount, 'times, which is the maximum amount accounting for letters occuring multiple times in a single word.')
 MaxLetterCountDistWithLetter = []
@@ -69,6 +74,7 @@ for letter in MaxLetterCountDistWithLetter:
 print('    ', 'Letter Order accounting for letters occuring multiple times in a single word:', MaxLetterCountLetterOrder[:-2][::-1])
 print('')
 
+# Determine the distribution of letters within a word in the dictionary (NOT counting multiples within a word)
 MaxLettersInWordCount = max(LettersInWordCount)
 print('The letter', InvLettersDict[LettersInWordCount.index(MaxLettersInWordCount)+1], 'achieves occurs', MaxLettersInWordCount, 'times, which is the maximum amount without accounting for letters occuring multiple times in a single word.')
 MaxLetterInWordCountDistWithLetter = []
@@ -82,17 +88,21 @@ print('    ', 'Letter Order without accounting for letters occuring multiple tim
 
 print('')
 
+# Determine what letters appear the most in a given position
+# for loop ranges over all positions (zero counting)
 for i in range(WordleLength):
+    # Distribution of letters as achieved from the above function definition.
     Distribution = LetterCountPosition(i)
 
-    plt.bar(axletters, Distribution, color ='forestgreen',
-        width = 0.4)
+    # Plot the distribution
+    plt.bar(axletters, Distribution, color ='forestgreen', width = 0.4)
     plt.xlabel("Letters")
     plt.ylabel("Words with that letter in position %s" %str(i))
     plt.title("Letter Appearance in Position %s" %str(i))
     plt.savefig('LetterDistributionPosition%s' %str(i))
     plt.close()
 
+    # Print the statistics reguarding each position.
     MaxDistribution = max(Distribution)
     LetterOrder = ''
     print('Position', str(i) + ':')
