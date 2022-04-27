ad485609b14f9d846f5b8659eebb996cade4f2d9
jmh0036 <jmh0036@tigermail.auburn.edu>
2022-04-27 19:37:29 -0400

    Adding Comments

--- WordleWords.py
+++ WordleWords.py
@@ -67,11 +67,15 @@ MaxLetterCount = max(LetterCount)
 print('The letter', InvLettersDict[LetterCount.index(MaxLetterCount)+1], 'achieves occurs', MaxLetterCount, 'times, which is the maximum amount accounting for letters occurring multiple times in a single word.')
 MaxLetterCountDistWithLetter = []
 MaxLetterCountLetterOrder = ''
+# Create a list (number of Occurrences, letter) to easily sort
 for idx,occurrence in enumerate(LetterCount):
     MaxLetterCountDistWithLetter.append([occurrence, InvLettersDict[idx+1]])
 MaxLetterCountDistWithLetter.sort()
+# Create a nice display of the list of letters
 for letter in MaxLetterCountDistWithLetter:
     MaxLetterCountLetterOrder += letter[1] + ' ,'
+# We want to cut off the ', ' from the very end
+# Because sort() goes from lowest to hightest, we want to reverse the list (most occuring to least)
 print('    ', 'Letter Order accounting for letters occurring multiple times in a single word:', MaxLetterCountLetterOrder[:-2][::-1])
 print('')
 
@@ -80,11 +84,15 @@ MaxLettersInWordCount = max(LettersInWordCount)
 print('The letter', InvLettersDict[LettersInWordCount.index(MaxLettersInWordCount)+1], 'achieves occurs', MaxLettersInWordCount, 'times, which is the maximum amount without accounting for letters occurring multiple times in a single word.')
 MaxLetterInWordCountDistWithLetter = []
 MaxLetterInWordCountLetterOrder = ''
+# Create a list (number of Occurrences, letter) to easily sort
 for idx,occurrence in enumerate(LettersInWordCount):
     MaxLetterInWordCountDistWithLetter.append([occurrence, InvLettersDict[idx+1]])
 MaxLetterInWordCountDistWithLetter.sort()
+# Create a nice display of the list of letters
 for letter in MaxLetterInWordCountDistWithLetter:
     MaxLetterInWordCountLetterOrder += letter[1] + ' ,'
+# We want to cut off the ', ' from the very end
+# Because sort() goes from lowest to hightest, we want to reverse the list (most occuring to least)
 print('    ', 'Letter Order without accounting for letters occurring multiple times in a single word:', MaxLetterInWordCountLetterOrder[:-2][::-1])
 
 print('')
@@ -112,12 +120,17 @@ for i in range(WordleLength):
     print('    ', 'Max in Dist: ', MaxDistribution)
     print('    ', 'Max Letter:  ', InvLettersDict[Distribution.index(MaxDistribution)+1])
     DistWithLetter = []
+    # Create a list (number of Occurrences, letter) to easily sort
     for idx,occurrence in enumerate(Distribution):
         DistWithLetter.append([occurrence, InvLettersDict[idx+1]])
     DistWithLetter.sort()
+    # Create a nice display of the list of letters
     for letter in DistWithLetter:
         LetterOrder += letter[1] + ' ,'
+    # We want to cut off the ', ' from the very end
+    # Because sort() goes from lowest to hightest, we want to reverse the list (most occuring to least)
     print('    ', 'Letter Order:', LetterOrder[:-2][::-1])
+    # We want to make a list of lists for later to find the weight of each word
     PositionDistributions.append(LetterOrder[:-2][::-1])
     print('')
 
@@ -126,13 +139,26 @@ for i in range(WordleLength):
 MinimumWeight = WordleLength*len(LettersDict)
 WordsWithMinWeight = []
 for word in WordleWords:
+    # Initialize / Reset weight
     wordWeight = 0
+    # Comb through each word
     for letteridx in range(WordleLength):
-        wordWeight += PositionDistributions[letteridx].index(word[letteridx])/3
+        # Add up the weight for each letter
+        # We need to divide by 3, because of how the displayed string is (#, #, ...)
+        wordWeight += PositionDistributions[letteridx].index(word[letteridx])//3
+    # If we have a new minimum weight, reset the minimum weight overall and the minimum weight word list.
     if wordWeight < MinimumWeight:
         MinimumWeight = wordWeight
         WordsWithMinWeight = [word]
+    # If there's another word with the minimum weight, add it to the minimum weight word list.
     if wordWeight == MinimumWeight:
         WordsWithMinWeight.append(word)
+    # These are my personal favorite starting words.  Had to see what their weight was.
+    if word == 'bread':
+        print('The word', word, 'has a weight of', wordWeight)
+    if word == 'roast':
+        print('The word', word, 'has a weight of', wordWeight)
 
-print(MinimumWeight,WordsWithMinWeight)
+print('')
+
+print('The words', WordsWithMinWeight, 'have smallest weight of', MinimumWeight)
